ba8b9412fa385a74efc5b230d4f3660e1232811f
Evgenio7779 <6757508@gmail.com>
2019-03-24 19:29:22 +0300

    feat: add brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  array = [];
+ 
+  for ( a = 0; a < bracketsConfig.length; a++) {
+  
+  array.push(bracketsConfig[a][0] + bracketsConfig[a][1]);
+ };
+
+ for ( b = 0; b < array.length + 1; b++) {
+
+     for ( c = 0; c < array.length; c++) {
+        
+        for ( x = 0; x < str.length; x++) {
+             
+            if (str.indexOf(array[c]) != -1) {
+                 
+                index = str.indexOf(array[c]);
+                 
+                str = str.substring(0 , index) + str.substring(2 + index);
+                
+                x--;
+             };
+         };
+     };
+ };
+
+ return str.length == 0 ? true : false;
 }
